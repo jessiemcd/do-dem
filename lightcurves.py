802e45d40bb3448b0aaf4f1d2b0ebd0b551919d0
jessiemcd <53271778+jessiemcd@users.noreply.github.com>
2023-11-21 11:33:22 -0500

    Add files via upload

--- lightcurves.py
+++ lightcurves.py
@@ -374,7 +374,7 @@ def plot_nustar_lightcurves(eranges = [[2.,4.],[4.,6.],[6.,10.]]):
                  **default_kwargs, color='Black')
     ax3.plot(times_convertedB, lvtB, 
                  label='NuSTAR FPMA Livetime',
-                 **default_kwargs,, color='Black', linestyle='dashed')
+                 **default_kwargs, color='Black', linestyle='dashed')
     
     #ax1.set_ylim(range1[0], range1[1])
     #ax1.set_xlim(timerange[0], timerange[1])
